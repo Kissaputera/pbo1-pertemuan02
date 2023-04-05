3922525e6d5f74920b82b4125a921c0279ba1bac
Kissaputera <mkissaputera@gmail.com>
2023-04-05 14:09:59 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("==Data Mahasiswa===========================");
+  System.out.println("NPM  : 2110010394");
+  System.out.println("Nama : Muhammad Rayhan Kissaputera");
+  System.out.println("===========================================");
+ }
+}
